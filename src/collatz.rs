18da0bd79d8591542c9c4458f7d22533ce75c39c
Dallas Strouse <dastrouses@gmail.com>
2023-03-25 14:40:42 -0500

    Add the Collatz Conjecture

---
+++ src/collatz.rs
@@ -0,0 +1,16 @@
+// The Collatz conjecture: https://en.wikipedia.org/wiki/Collatz_conjecture
+pub fn collatz(mut num: u32) {
+    // The Collatz conjecture only works properly on positive integers; u32 only allows positive integers.
+    // Therefore, we don't need to handle if a negative number (or float) is plugged in.
+
+    loop {
+        println!("{num}");
+        // When the integer hits the 16 -> 8 -> 4 -> 2 -> 1 range (and it will) we can exit.
+        // This also helps to make sure we don't do any unnecessary calculations.
+        if num == 1 {
+            return ();
+        };
+        // If num is even, divide by two; else, multiply by three and add one.
+        num = if 0 == num % 2 { num / 2 } else { 3 * num + 1 };
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod collatz;
+
 fn main() {
     println!("Hello, world!");
+    // TODO: We should ask for user input, and take CLI arguments to see what math funtions the user wants to do.
+    collatz::collatz(54);
 }
